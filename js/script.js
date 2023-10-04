94cc46da062958347b2367e6bb450590594170dc
wade199 <ibrahima97wade99@gmail.com>
2023-10-04 15:38:03 +0200

    deuxiéme commit

--- js/script.js
+++ js/script.js
@@ -2,5 +2,13 @@ var av = document.getElementById("avatar");
 console.log(avatar);
 av.addEventListener("click",avClick, false);
 function avClick(){
-   alert("avatar");
+  console.log(avatar.innerText); 
+   alert (avatar.innerText);
+}
+
+var av = document.getElementById ('avatar');
+var ul = ul.length[0].length;
+for(let item of ul){
+    var ul = item.length;
+    console.log(avatar.innerText);  
 }
